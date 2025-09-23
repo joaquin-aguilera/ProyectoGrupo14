fa65617526e5cdb9776bec1df9eb84e76412cf49
Rodrigo <107264333+Rodrigo189@users.noreply.github.com>
2025-09-23 11:46:12 -0300

    Prueba para Railway

--- start.sh
+++ start.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# Navegar al frontend
-cd mi_web/frontend
-
-npm install
-
-# Compilar frontend React
-npm run build
-
-# Volver al backend
-cd ../backend
-
-# Levantar Flask y servir los archivos de frontend
+# Prueba para Backend
 export FLASK_APP=app.py
 export FLASK_ENV=development
 flask run --host=0.0.0.0 --port=5000
